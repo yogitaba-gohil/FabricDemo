bc2cb7fd700988f398b851b6e147bb10ecdedfe6
Yogitaba Gohil <yogitabagohil1212@gmail.com>
2022-12-23 09:40:59 +0200

    button api 1 added

--- client/src/components/buttonLayout/ButtonLayout.js
+++ client/src/components/buttonLayout/ButtonLayout.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import './ButtonLayout.css'
 import Button from '@mui/material/Button';
+import axios from 'axios';
+
+
 
 
 
 const ButtonLayout = () => {
+
+  const onButtonClick = () =>{
+    axios({  
+      method: 'post',  
+      url: 'http://localhost:3000/api/v1/fetch',  
+      data: {"fetchApi": 1}
+    });
+  }
   return (
-    <div className='container'><Button variant="contained">Contained</Button>
+    <div className='container'><Button variant="contained" onClick={onButtonClick}>Contained</Button>
     <Button variant="contained">Contained</Button>
     <Button variant="contained">Contained</Button>
     </div>
