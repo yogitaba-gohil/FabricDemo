6677efab0b64f998b5cd35a10b62154a754856de
Yogitaba Gohil <yogitabagohil1212@gmail.com>
2022-12-25 14:00:25 +0200

    button API set

--- client/src/components/buttonLayout/ButtonLayout.js
+++ client/src/components/buttonLayout/ButtonLayout.js
@@ -1,27 +1,70 @@
-import React from 'react'
-import './ButtonLayout.css'
-import Button from '@mui/material/Button';
-import axios from 'axios';
-
-
-
-
+import React, { useState } from "react";
+import "./ButtonLayout.css";
+import Button from "@mui/material/Button";
+import axios from "axios";
 
 const ButtonLayout = () => {
+  const [data, setData] = useState([]);
 
-  const onButtonClick = () =>{
-    axios({  
-      method: 'post',  
-      url: 'http://localhost:3000/api/v1/fetch',  
-      data: {"fetchApi": 1}
-    });
-  }
+  const onButtonClick = (e) => {
+    console.log("e", e.target.value);
+    if (e.target.value === "firstButton") {
+      axios({
+        method: "post",
+        url: "/api/v1/fetch",
+        data: { fetchApi: 1 },
+      }).then((res) => {
+        const resData = res.data;
+        setData(resData.data.Search);
+      });
+    } else if (e.target.value === "secondButton") {
+      axios({
+        method: "post",
+        url: "/api/v1/fetch",
+        data: { fetchApi: 2 },
+      }).then((res) => {
+        const resData = res.data;
+        setData(resData.data.Search);
+      });
+    } else {
+      axios({
+        method: "post",
+        url: "/api/v1/fetch",
+        data: { fetchApi: 3 },
+      }).then((res) => {
+        const resData = res.data;
+        setData(resData.data.Search);
+      });
+    }
+  };
   return (
-    <div className='container'><Button variant="contained" onClick={onButtonClick}>Contained</Button>
-    <Button variant="contained">Contained</Button>
-    <Button variant="contained">Contained</Button>
+    <>
+  
+    <div className="container">
+      <Button
+        variant="contained"
+        value="firstButton"
+        onClick={(e) => onButtonClick(e)}
+      >
+        Contained
+      </Button>
+      <Button
+        variant="contained"
+        value="secondButton"
+        onClick={(e) => onButtonClick(e)}
+      >
+        Contained
+      </Button>
+      <Button
+        variant="contained"
+        value="thirdButton"
+        onClick={(e) => onButtonClick(e)}
+      >
+        Contained
+      </Button>
     </div>
-  )
-}
+    </>
+  );
+};
 
-export default ButtonLayout
+export default ButtonLayout;
